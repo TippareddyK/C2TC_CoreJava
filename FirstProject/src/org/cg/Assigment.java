b290af1178f6fb33bd8b212d2e5b6ff36384096e
tippa <tippa@reddy>
2022-08-04 08:56:08 +0530

    first commit

--- FirstProject/src/org/cg/Assigment.java
+++ FirstProject/src/org/cg/Assigment.java
@@ -3,25 +3,25 @@ package org.cg;
 public class Assigment {
 	
 	public static void main(String[] args) {
-		float salary = 450000;
-		if (salary > 1000000) {
+		float b = 450000;
+		if (b > 1000000) {
 			float a = (float)30/100;
-			float tax = a*salary;
+			float taxc = a*b;
 			System.out.print("Tax to be paid = ");
-			System.out.println(tax);
+			System.out.println(taxc);
 		}
-		else if (salary > 500000 & salary <= 1000000) {
+		else if (b > 500000 & b <= 1000000) {
 			float a = (float)20/100;
-			float tax = a*salary;
+			float taxc = a*b;
 			System.out.print("Tax to be paid = ");
-			System.out.println(tax);
+			System.out.println(taxc);
 			
 		}
 		else {
 			float a = (float)5/100;
-			float tax = a*salary;
+			float taxc = a*b;
 			System.out.print("Tax to be paid = ");
-			System.out.println(tax);
+			System.out.println(taxc);
 				
 		}
 		
