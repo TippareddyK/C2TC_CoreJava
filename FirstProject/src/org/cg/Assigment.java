b18bd6b71cbaf3960b46d332d61fa731d7f4a0a8
tippa <tippa@reddy>
2022-08-04 08:59:29 +0530

    Merge branch 'master' of https://github.com/TippareddyK/C2TC_CoreJava.git

--- FirstProject/src/org/cg/Assigment.java
+++ FirstProject/src/org/cg/Assigment.java
@@ -3,25 +3,25 @@ package org.cg;
 public class Assigment {
 	
 	public static void main(String[] args) {
-		float b = 450000;
-		if (b > 1000000) {
+		float salary = 450000;
+		if (salary > 1000000) {
 			float a = (float)30/100;
-			float taxc = a*b;
+			float tax = a*salary;
 			System.out.print("Tax to be paid = ");
-			System.out.println(taxc);
+			System.out.println(tax);
 		}
-		else if (b > 500000 & b <= 1000000) {
+		else if (salary > 500000 & salary <= 1000000) {
 			float a = (float)20/100;
-			float taxc = a*b;
+			float tax = a*salary;
 			System.out.print("Tax to be paid = ");
-			System.out.println(taxc);
+			System.out.println(tax);
 			
 		}
 		else {
 			float a = (float)5/100;
-			float taxc = a*b;
+			float tax = a*salary;
 			System.out.print("Tax to be paid = ");
-			System.out.println(taxc);
+			System.out.println(tax);
 				
 		}
 		
